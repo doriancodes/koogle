942a6600a7f3358b0629841707862812afacc670
doriancodes <hello@alo.codes>
2020-08-26 16:37:42 +0200

    edited build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jsoup:jsoup:1.13.1")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
