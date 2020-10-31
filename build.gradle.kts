f271ebdc4faaf4731f5b569196f7ed17c221c43b
doriancodes <hello@alo.codes>
2020-10-31 20:49:11 +0100

    added vaadin to handle ui

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.3.2.RELEASE"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	id("com.vaadin") version "0.17.0.1"
 	kotlin("jvm") version "1.3.72"
 	kotlin("plugin.spring") version "1.3.72"
 	kotlin("plugin.jpa") version "1.3.72"
@@ -23,6 +24,11 @@ repositories {
 	mavenCentral()
 }
 
+
+ext {
+	setOf("vaadinVersion", "17.0.3")
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-mustache")
@@ -31,6 +37,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jsoup:jsoup:1.13.1")
+	implementation("com.vaadin:vaadin-spring-boot-starter")
 	runtimeOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
@@ -38,8 +45,18 @@ dependencies {
 	}
 }
 
+dependencyManagement {
+	imports {
+		mavenBom("com.vaadin:vaadin-bom:17.0.3")
+	}
+}
+
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+vaadin {
+	pnpmEnable = true
 }
 
 tasks.withType<KotlinCompile> {
